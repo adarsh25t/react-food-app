49c44a34a3d5b4f7b017a6eb642dd3fdf1d7cc93
ADARSH <adarsh25t@gmail.com>
2021-10-09 15:00:24 +0530

    headercard

---
+++ src/components/HeaderCard/HeaderCard.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './HeaderCard.css'
+function HeaderCard(props) {
+
+   console.log(props.SpeacialMenu);
+    
+
+    return (
+        <div className="headercard">
+           
+        </div>
+    )
+}
+
+export default HeaderCard
---
+++ src/components/HeaderCard/cardDetails.js
@@ -0,0 +1,4 @@
+
+let cardDetails = [{
+    ItemName : ""
+}]
